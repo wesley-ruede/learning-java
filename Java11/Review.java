23af2822b842d19af061f1e8e5ecffb45364b5a2
wesley-ruede <wesley.k.r1993@gmail.com>
2019-08-29 19:18:45 -0400

    my first java app

---
+++ Java11/Review.java
@@ -0,0 +1,6 @@
+public class Review { // Java requires a class with the name of the file
+	public static void main(String[] args) { // Java requires a main() method/function
+		// remember the semicolon!
+		System.out.println("My first Java program on my own");
+	} // end of main() function
+} // closing the class
---
+++ Java11/afile.java
@@ -0,0 +1,5 @@
+public class afile {
+	public static void main(String[] args){
+		System.out.println("hello Wes");
+	}
+}
