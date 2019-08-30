0262d6cb594dee7d1aaf9f79dd4d4366f7c69018
wesley-ruede <wesley.k.r1993@gmail.com>
2019-08-30 00:07:15 -0400

    planting trees with Java

---
+++ Java11/Tree.java
@@ -0,0 +1,27 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // a method to introduce myself
+    System.out.println("Hey I'm Wes and I love coding!");
+    System.out.println("I'm gonna plant a tree today");
+    System.out.println("   *   ");
+    System.out.println("  ***  ");
+    System.out.println("  ***  ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("Yay lets plant more trees!");
+    System.out.println("* ** *");
+    System.out.println("******");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  ");
+    System.out.println("~~~~~~~~~");
+    System.out.println("Let's save the Amazon!");
+    System.out.println("***** ");
+    System.out.println("***** ");
+    System.out.println("***** ");
+    System.out.println("  *   ");
+    System.out.println("  *   ");
+    System.out.println("######");
+  }
+}
