f66f1d864d1b617dbb5828a7eb890f77534a6770
wesley-ruede <wesley.k.r1993@gmail.com>
2019-09-04 07:29:47 -0400

    more java skills 18 of 25 hours left

---
+++ Java11/MathMagic.java
@@ -0,0 +1,14 @@
+// Operator practice
+public class MathMagic {
+  public static void main(String[] args) {
+	      int myNumber = 3;
+    	      // myNumber is the original number
+    	      int stepOne = myNumber * myNumber;
+	      int stepTwo = stepOne + myNumber;
+   	      int stepThree = stepTwo / myNumber;
+	      int stepFour = stepThree + 17;
+	      int stepFive = stepFour - myNumber;
+	      int stepSix = stepFive / 6;
+	      System.out.println(stepSix);
+  }
+}
---
+++ Java11/OOPandObjects.java
@@ -0,0 +1,18 @@
+public class OOPandObjects{
+	String duckFoot;
+	int duckWings;
+	int duckBeak;
+
+	public OOPandObjects(String foot, int wings, int beak){
+		duckFoot = foot;
+		duckWings = wings;
+		duckBeak = beak;
+	}
+
+	public static void main(String[] args) {
+		OOPandObjects Duck = new OOPandObjects("Ducks have two feet", 2, 1);
+		System.out.println(Duck.duckFoot);
+		boolean duckParts = Duck.duckWings >= Duck.duckBeak;
+		System.out.println("It is " +duckParts+ " that ducks have more wings than beaks");
+	}
+}
