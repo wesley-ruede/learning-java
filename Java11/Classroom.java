e724f462282e06b8f92902ece41ea36d8e0b0ffa
wesley-ruede <wesley.k.r1993@gmail.com>
2019-09-05 13:30:56 -0400

    args parameter and review of arrays

---
+++ Java11/Classroom.java
@@ -0,0 +1,19 @@
+// working with arrays review
+import java.util.Arrays;
+
+public class Classroom {
+  
+  public static void main(String[] args){
+    // creating and assigning values to a string array
+    String[] students = {"Sade", "Alexus", "Sam", "Koma"};
+    // creating a new empty array
+    double[] mathScores = new double[4];
+    // setting values in respect to the students 
+    // Sade
+    mathScores[0] = 94.5;
+    // Sam
+    mathScores[2] = 76.8;
+    // getting the length of students and printing it
+    System.out.println("The number of students in the class is " + students.length);
+  }
+}
